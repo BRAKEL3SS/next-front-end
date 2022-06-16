07cc328f128f01844ac0860ad3b4f63a5d2603c7
brakel3ss <rck496@yahoo.com>
2022-06-16 15:22:38 -0700

    adds custom 404 page and showcase component

---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Showcase.module.css'
+
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+        <h1>Welcome to the racing</h1>
+        <h2>Find the best events</h2>
+    </div>
+  )
+}
